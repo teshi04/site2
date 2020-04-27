03ad4e04dbe14aab202e2295273aac382e828207
teshi04 <teshi04@gmail.com>
2020-04-27 23:59:41 +0900

    :zap: Add nekouo and usagi

---
+++ static/usagi/assets/js/main.js
@@ -0,0 +1,40 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+renderer.setClearColor(0xffab40, 1);
+renderer.gammaOutput = true;
+
+var light = new THREE.DirectionalLight("#c1582d", 1);
+var ambient = new THREE.AmbientLight("#85b2cd");
+light.position.set(40, 70, 90).normalize();
+scene.add(light);
+scene.add(ambient);
+
+var loader = new THREE.GLTFLoader();
+loader.load('assets/model/usagi.glb', function (gltf) {
+    scene.add(gltf.scene);
+    renderer.render(scene, camera);
+}, undefined, function (error) {
+    console.error(error);
+});
+
+camera.position.x = -10;
+camera.position.y = 20;
+camera.position.z = -50;
+camera.lookAt(new THREE.Vector3(0, 5, 0));
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.autoRotate = true;
+
+tick();
+
+function tick() {
+    controls.update()
+    controls.autoRotateSpeed = 10
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+}
