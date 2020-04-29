11c7cef5dfd3ef7757978cbbaf8eba8ec5931c2b
teshi04 <teshi04@gmail.com>
2020-04-29 17:39:29 +0900

    :zap: Add fab

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { MDCRipple } from '@material/ripple/index'
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'))
-
-console.log('hello world')
+const selector = '.mdc-button, .content-card, .mdc-card__primary-action'
+const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
+  return new MDCRipple(el)
+})
